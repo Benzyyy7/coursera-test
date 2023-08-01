127bc0a9fa643785737d4692ce4834ec0da43a98
Nigesh Byanjankar <67941150+Benzyyy7@users.noreply.github.com>
2023-08-01 15:34:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 console.log("Hello, PowerShell and JavaScript!");
-for (let i = 0;i<10;i++){
+for (let i = 0; i < 10; i++) {
   console.log(i);
 }
-function addTwoNum(a,b){
-  return a+b;
+function addTwoNum(a, b) {
+  return a + b;
 }
+
+let car = {
+  color: red,
+  speed: fast,
+  seat: 4,
+};
+
+console.log(car.color);
